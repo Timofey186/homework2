40c217e740be29649bd16a451aa1b27ed84654cf
Timofey186 <popov.timofey.my@gmail.com>
2024-07-16 19:08:51 +0500

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,4 @@
+Completed_tasks = 12
+Hours_spent = 1.5
+Cours = 'Python'
+print(F"Курс: {Cours}, всего задач: {Completed_tasks}, затрачено вермени: {Hours_spent}, среднее время выполнения:", Hours_spent / Completed_tasks, "часа")
